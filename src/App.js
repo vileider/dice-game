56ff68601b2a6ecfaf94fd738bf5e9bca968d251
vilider <andrewbagio@yahoo.com>
2020-11-13 15:48:06 +0000

    results reset & beginigof high score

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import DiceGame from "./DiceGame";
 
+
 function App() {
     return (
+        
         <div className="App">
             <DiceGame></DiceGame>
+            
         </div>
-    );
+        
+    )
 }
 
 export default App;
--- src/DiceGame.js
+++ src/DiceGame.js
@@ -1,6 +1,8 @@
 import './DiceGame.css';
+import './App.css';
 import Dice from "./Dice";
 import {useState} from 'react';
+
 
 function DiceGame() {
     const [areDicesRolling, setAreDicesRolling] = useState(false);
@@ -8,9 +10,11 @@ function DiceGame() {
     const [playerSecondDiceCurrentNumber, setPlayerSecondDiceCurrentNumber] = useState(1);
     const [computerFirstDiceCurrentNumber, setComputerFirstDiceCurrentNumber] = useState(1);
     const [computerSecondDiceCurrentNumber, setComputerSecondDiceCurrentNumber] = useState(1);
-
+    const [playerScore, setPlayerScore] = useState(0);
+    const [computerScore, setComputerScore] = useState(0);
     const getRandomDiceResult = () => Math.floor(Math.random() * 6) + 1;
 
+    
     function randomizeDices() {
         setAreDicesRolling(true);
         const dicesRollingTimeout = 2000;
@@ -29,7 +33,7 @@ function DiceGame() {
     }
 
     function getComputerScore() {
-        return computerFirstDiceCurrentNumber + computerSecondDiceCurrentNumber;
+        return computerFirstDiceCurrentNumber + computerSecondDiceCurrentNumber ;
     }
 
     function getWinner() {
@@ -41,8 +45,14 @@ function DiceGame() {
             return <div>Draw!</div>;
         }
     }
-
+    function resetGame(){
+        setPlayerFirstDiceCurrentNumber(0)
+        setPlayerSecondDiceCurrentNumber(0)
+        setComputerFirstDiceCurrentNumber(0)
+        setComputerSecondDiceCurrentNumber(0)
+    }
     return (
+        <>
         <div className="DiceGame">
             {areDicesRolling === false ? <div>
                 <div className="PlayerDices">
@@ -55,16 +65,23 @@ function DiceGame() {
                     Computer dices:
                     <Dice currentNumber={playerFirstDiceCurrentNumber}></Dice>
                     <Dice currentNumber={playerSecondDiceCurrentNumber}></Dice>
-                    Computer result: {getComputerScore()}
+                    Computer result: {getComputerScore() }
                 </div>
                 <div className="WinnerContainer">
                     {getWinner()}
                 </div>
             </div> : <div>Dices are rolllling....</div>}
             <div className="ThrowButton">
-                <button disabled={areDicesRolling} onClick={randomizeDices}>Throw dice</button>
+                <button disabled={areDicesRolling} 
+                onClick={randomizeDices}>Throw dice</button>
+                <button onClick={resetGame}>Reset Game</button>
             </div>
         </div>
+        
+        <div className="Score">
+            {playerScore}
+        </div>
+        </>
     );
 }
 
